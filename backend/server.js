0ddb2d140d728b63587c3e44fe685ab02f97012c
Harry-Sunil <harrysunilh4@gmail.com>
2026-05-28 20:08:54 +0530

    fixed upload issue

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(express.json());
+
 app.use("/uploads", express.static("uploads"));
 app.use("/student", studentRoutes);
 app.use("/admin", adminRoutes);
@@ -38,7 +38,11 @@ app.get("/", (req, res) => {
   res.send("Campus Recruitment Backend Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
