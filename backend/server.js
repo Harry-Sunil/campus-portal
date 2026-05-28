050bdd4e88c370185e28f811e381cad6fe5e982e
Harry-Sunil <harrysunilh4@gmail.com>
2026-05-28 19:44:03 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
